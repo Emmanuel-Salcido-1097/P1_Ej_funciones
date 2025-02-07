8a1a6729125830abe3a78028f040e583adc63548
Emmanuel-Salcido-1097 <164534125+Emmanuel-Salcido-1097@users.noreply.github.com>
2025-02-07 16:47:42 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+// funcion que no devuelve valor
+// funcion saludar
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('Emmanuel Salcido Ortega');
+}
+
+void main(){
+  // Emmanuel Salcido Ortega Mat:22308051281097
+  // Llamar a la funcion saludar
+  print('Emmanuel Salcido Ortega Mat:22308051281097');
+  print('Llamando a la funcion saludar');
+  saludar();
+  print('Llamando a la funcion escribeNombre');
+  escribeNombre();
+
+}
